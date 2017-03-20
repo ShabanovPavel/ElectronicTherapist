0b3770d201518238e6754093a334f5a600108e88
ShabanovPavel <shabanov.pavel.andreyevich@gmail.com>
2017-03-20 20:48:04 +0700

    Проверка Mocha.js

--- test/test.js
+++ test/test.js
@@ -1,3 +1,11 @@
 /**
  * Created by FIRCorp on 20.03.2017.
  */
+var assert = require('assert');
+describe('TherapistStart', function() {
+    describe('#Check job', function() {
+        it('should return error', function() {
+            assert.equal(4,2);
+        });
+    });
+});
