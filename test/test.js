e147e3af29ee4bbad590bd488de0557cbafb79c8
ShabanovPavel <shabanov.pavel.andreyevich@gmail.com>
2017-03-20 21:02:04 +0700

    Тест на создание класса

--- test/test.js
+++ test/test.js
@@ -2,10 +2,19 @@
  * Created by FIRCorp on 20.03.2017.
  */
 var assert = require('assert');
-describe('TherapistStart', function() {
-    describe('#Check job', function() {
-        it('should return error', function() {
-            assert.equal(4,2);
+
+describe('TherapistStart', function () {
+    describe('#Check job', function () {
+        it('should return error', function () {
+            assert.equal(4, 2);
+        });
+    });
+
+    describe('#Check class Main', function () {
+        it('should return true, if class create ', function () {
+            assert.ok( new Main());
         });
     });
 });
+
+
