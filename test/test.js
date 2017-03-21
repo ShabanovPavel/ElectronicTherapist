258b6310563ecd30cab8e0f98e7751008923380c
ShabanovPavel <shabanov.pavel.andreyevich@gmail.com>
2017-03-21 21:45:53 +0700

    Тесты на создание позаболеванию  массива ссылок на вопросы глобальной базы с упоминанием этого заболевания

--- test/test.js
+++ test/test.js
@@ -18,13 +18,13 @@ describe('TherapistStart', () => {
 
     describe('#Check class Question', () => {
         it('should return true, if class create ', () => {
-            assert.ok(new Question());
+            assert.ok(new Question({}));
         });
     });
 
     describe('#Check class Catalog', () => {
         it('should return true, if class create ', () => {
-            assert.ok(new Catalog());
+            assert.ok(new Catalog([]));
         });
     });
 });
@@ -33,72 +33,33 @@ describe('Therapist', () => {
     describe('#Check class Catalog', () => {
         describe('#Check list of diseases', () => {
             it('correctness of withdrawal', () => {
-                let catalog = new Catalog();
+                let catalog = new Catalog([]);
                 assert.equal(TypeDiseases.ORV, catalog.getTypeDiseases());
             });
 
         });
 
-        describe('#Check matrix Question', () => {
-            it('checking the array of questions', () => {
-                let catalog = new Catalog();
-                assert.equal(1, catalog.getArrayQuestion()[0]);
-            });
-        });
 
         describe('#Check constructor Catalog', () => {
             it('create a global catalog of questions', () => {
-                const baseQuestion = [
-                    {
-                        wording: 'У вас есть температура?',
-                        yes: [
-                            TypeDiseases.ORV
-                        ],
-                        no: [
-                            TypeDiseases.ORZ
-                        ]
-                    },
-                    {
-                        wording: 'У вас есть насморк?',
-                        yes: [
-                            TypeDiseases.ORZ
-                        ],
-                        no: [
-                            TypeDiseases.ORV
-                        ]
-                    },
-                ];
+
                 let catalog = new Catalog(baseQuestion);
-                let obj=baseQuestion[0];
+                let obj = baseQuestion[0];
                 assert.equal(obj.wording, catalog.getArrayQuestion()[0].wording);
-                assert.equal(obj.yes, catalog.getArrayQuestion()[0].yes);
-                assert.equal(obj.no, catalog.getArrayQuestion()[0].no);
+                assert.equal(obj.yes, catalog.getArrayQuestion()[0].answerYes);
+                assert.equal(obj.no, catalog.getArrayQuestion()[0].answerNo);
+            });
+
+            it('check create matrix for links on global matrix', () => {
+
+                let catalog = new Catalog(baseQuestion);
+                assert.equal([], catalog.getArrayLinksOnQuestion(TypeDiseases.ORZ));
+
             });
         });
     });
 
-    describe('#Check class Question', () => {
 
-        it('checking wording constructor', () => {
-            let wording = 'Вы испытываете головную боль';
-            let question = new Question(wording);
-            assert.equal(wording, question.wording);
-        });
-
-        it('checking answerYes constructor', () => {
-            let yes = [TypeDiseases.ORZ, TypeDiseases.ORV];
-            let question = new Question(null, yes);
-            assert.equal(yes, question.answerYes);
-        });
-
-        it('checking answerNo constructor', () => {
-            let no = [TypeDiseases.flu];
-            let question = new Question(null, null, no);
-            assert.equal(no, question.answerNo);
-        });
-
-
-    });
 });
 
 /**
@@ -115,14 +76,13 @@ class Question {
 
     /**
      * Constructor of the Question
-     * @param wording {string} формулировка вопроса
-     * @param answerYes {Array<TypeDiseases>} массив заболеваний которые подозреваются при ответе на вопрос ответом да
-     * @param answerNo {Array<TypeDiseases>} массив заболеваний которые подозреваются при ответе на вопрос ответом нет
+     * @param item {{wording: string, yes: Array<TypeDiseases>, no: Array<TypeDiseases>}}
      */
-    constructor(wording, answerYes, answerNo) {
+    constructor(item) {
+        let {wording, yes, no}=item;
         this.wording = wording;
-        this.answerYes = answerYes;
-        this.answerNo = answerNo;
+        this.answerYes = yes;
+        this.answerNo = no;
     }
 
     /**
@@ -157,8 +117,11 @@ class Catalog {
     /**
      * Constructor of the Catalog
      */
-    constructor() {
-        this.arrayQuestion = [1];
+    constructor(baseQuestion) {
+        this.arrayQuestion=[];
+        baseQuestion.forEach((item) => {
+            this.arrayQuestion.push(new Question(item));
+        });
     }
 
     /**
@@ -187,3 +150,28 @@ const TypeDiseases = {
     ORV: 1,
     flu: 2,
 };
+
+/**
+ * База вопросов
+ * @type {[*]}
+ */
+const baseQuestion = [
+    {
+        wording: 'У вас есть температура?',
+        yes: [
+            TypeDiseases.ORV
+        ],
+        no: [
+            TypeDiseases.ORZ
+        ]
+    },
+    {
+        wording: 'У вас есть насморк?',
+        yes: [
+            TypeDiseases.ORZ
+        ],
+        no: [
+            TypeDiseases.ORV
+        ]
+    },
+];
